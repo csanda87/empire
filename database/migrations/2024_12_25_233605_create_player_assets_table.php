523fa849a89b37f41eeb9e6d5756319dbb46be96
Chad Sanda <csanda87@gmail.com>
2025-01-02 20:27:44 -0600

    tweak migration

--- database/migrations/2024_12_25_233605_create_player_assets_table.php
+++ database/migrations/2024_12_25_233605_create_player_assets_table.php
@@ -14,11 +14,12 @@ return new class extends Migration
         Schema::create('player_assets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('player_id')->constrained();
-            $table->string('itemable_type'); // 'property', 'card',
-            $table->unsignedBigInteger('itemable_id');
+            $table->morphs('itemable');
             // $table->timestamps();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            
+            $table->index(['itemable_id', 'itemable_type']);
         });
     }
 
