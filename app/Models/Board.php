7494f14c7f81f2b2523916bbd7ac43bfc1802cf2
Chad Sanda <csanda87@gmail.com>
2025-08-10 19:53:08 -0500

    wip

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    protected $fillablle = [
+    protected $fillable = [
         'name',
         'description',
         'created_by',
